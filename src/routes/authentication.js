533049feb69521f460e60cad6dc4c5bf0d73dcd1
Eric-Alarcon441 <ericalarcon441@gmail.com>
2021-09-10 19:56:20 -0500

    Registro/signup de usuarios en mysql

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
-module.exports = router; 
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+
+router.post(
+  "/signup",
+  passport.authenticate("local.signup", {
+    successRedirect: "/profile",
+    failureRedirect: "/sigunp",
+    failureFlash: true,
+  })
+);
+
+router.get("/profile", (req, res) => {
+  res.send("this is your profile");
+});
+
+module.exports = router;
